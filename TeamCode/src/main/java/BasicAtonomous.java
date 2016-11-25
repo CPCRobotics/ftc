a7e41b830296f876288edc0e380ff5eeb7ca2778
Richard Elmore <richardelmore@live.com>
2016-11-25 15:33:25 -0800

    atonomous update
    correct circumference on weels and try to shoot

--- TeamCode/src/main/java/BasicAtonomous.java
+++ TeamCode/src/main/java/BasicAtonomous.java
@@ -15,39 +15,48 @@ public class BasicAtonomous extends OpMode
     DcMotor driveLeft   = null;
     DcMotor  driveRight  = null;
     //servo declaration
-    Servo servo = null;
+    Servo locker = null;
+    HardwareMap hwMap = super.hardwareMap;
     @Override
     public void init()
     {
-        HardwareMap hwMap = super.hardwareMap;
         //initalize drive motors
-        driveLeft   = hwMap.dcMotor.get("driveLeft");
-        driveRight  = hwMap.dcMotor.get("driveRight");
+        //driveLeft   = hwMap.dcMotor.get("driveLeft");
+        //driveRight  = hwMap.dcMotor.get("driveRight");
         // initalize servo
         //servo = hwMap.servo.get("servoExtendRight");
         //correct for motors being on oposite sides
-        driveLeft.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
-        driveRight.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+        //driveLeft.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
+       // driveRight.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
         // set inital positions and speeds
-        driveLeft.setPower(0);
-        driveRight.setPower(0);
+      //  driveLeft.setPower(0);
+        //driveRight.setPower(0);
         //servo.setPosition(0.0);
         // use encoders 1440 ticks pre rev for tetrix 1220 for andymark
-        driveLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        driveRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+      //  driveLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //driveRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //locker = hwMap.servo.get("locker");
+        HardwareMap hwMap = super.hardwareMap;
+        Servo locker = hwMap.servo.get("locker");
+        locker.setPosition(0.5);
     }
 
     @Override
     public void start()
     {
-        AL.DriveForwardEncoder(70,driveRight,driveLeft);//half the feild forward
+       /* AL.DriveForwardEncoder(70,driveRight,driveLeft);//half the feild forward
         AL.TurnLeftEncoders(90,driveRight,driveLeft); // turn to face becon
-        AL.DriveForwardEncoder(68,driveRight,driveLeft); //aproach beacon
+        AL.DriveForwardEncoder(68,driveRight,driveLeft); //aproach beacon*/
+        HardwareMap hwMap = super.hardwareMap;
+        Servo locker = hwMap.servo.get("locker");
+        locker.setPosition(0.5);
     }
 
     @Override
     public void loop()
     {
+        Servo locker = hwMap.servo.get("locker");
+        locker.setPosition(0.5);
         // some code using light sensor to hit all the beacons
     }
 }
--- TeamCode/src/main/java/EagleBotMethodLibrary.java
+++ TeamCode/src/main/java/EagleBotMethodLibrary.java
@@ -74,7 +74,7 @@ public class EagleBotMethodLibrary
     public void DriveForwardEncoder(int dist, DcMotor DR, DcMotor DL)
     {
         //constants to calculate dist in ticks assuming dist and cir are same units
-        int cir = 4,tickConstant=1440,ticks;
+        int cir = 12,tickConstant=1440,ticks;
 
         //calculate dist in ticks
         ticks = tickConstant*dist/cir;
@@ -96,7 +96,7 @@ public class EagleBotMethodLibrary
     public void DriveBackwardEncoders(int dist, DcMotor DR, DcMotor DL)
     {
         //constants to calculate dist in ticks assuming dist and cir are same units
-        int cir = 4,tickConstant=1440,ticks;
+        int cir = 12,tickConstant=1440,ticks;
 
         //calculate dist in ticks
         ticks = tickConstant*dist/cir;
@@ -114,7 +114,6 @@ public class EagleBotMethodLibrary
         //run full speed in reverse
         DR.setPower(-1);
         DL.setPower(-1);
-
     }
     //******if 1/2 rev in each wheal results in a 90 deg turn then A(deg)=A/180 rev************
     //ticks needed = A/180*1440
