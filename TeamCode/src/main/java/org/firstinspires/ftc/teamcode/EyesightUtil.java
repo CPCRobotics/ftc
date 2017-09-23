46669d32acce709f66b52542fe2a7bfe79ec862a
Samuel Hunter <lawful.lazy@gmail.com>
2017-09-23 10:30:11 -0700

    Delegate lower-level Vuphoria functions to EyesightUtil

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EyesightUtil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EyesightUtil.java
@@ -15,38 +15,55 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
  * Created by samuel on 9/22/17.
  */
 
+/**
+ * Utitiy class for Vuphoria image recognition
+ */
+
 class EyesightUtil {
 
-    VuforiaLocalizer vuforia;
-    VuforiaTrackables relicTrackables;
-    VuforiaTrackable relicTemplate;
+    static VuforiaLocalizer vuforia;
+    static VuforiaTrackables relicTrackables;
+    static VuforiaTrackable relicTemplate;
 
-    void init(OpMode opMode) {
+    static void init(OpMode opMode) {
         HardwareMap hardwareMap = opMode.hardwareMap;
 
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
 
+        /*
+         * WARNING! The git repository does not automatically hold the vuphoria license key, and so
+         * this will AUTOMATICALLY GIVE A BUILD ERROR!
+         *
+         * In the TeamCode folder, create a file `res/values/auth.xml` with this code snippet:
+         *
+         *     <?xml version="1.0" encoding="utf-8"?>
+         *     <resources>
+         *        <string name="vuphoria">abc/123</string>
+         *     </resources>
+         *
+         * Then replace "abc/123" with the correct vuphoria license key.
+         */
         parameters.vuforiaLicenseKey =  hardwareMap.appContext.getString(R.string.vuphoria);
 
         parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
         vuforia = ClassFactory.createVuforiaLocalizer(parameters);
 
-        relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        relicTrackables = vuforia.loadTrackablesFromAsset("RelicVuMark");
         relicTemplate = relicTrackables.get(0);
         relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
 
     }
 
-    void start() {
+    static void start() {
         relicTrackables.activate();
     }
 
-    RelicRecoveryVuMark getPictograph() {
+    static RelicRecoveryVuMark getPictograph() {
         return RelicRecoveryVuMark.from(relicTemplate);
     }
 
-    OpenGLMatrix getPose() {
+    static OpenGLMatrix getPose() {
         return ((VuforiaTrackableDefaultListener)relicTemplate.getListener()).getPose();
     }
 }
