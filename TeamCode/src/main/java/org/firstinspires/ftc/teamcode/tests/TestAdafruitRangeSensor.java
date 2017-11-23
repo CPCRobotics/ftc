7e6f3ce201c61f626bc0b091d7223f934e6a8bda
Jamie Hunter <jamie+git@thehunters.org>
2017-11-22 19:01:14 -0800

    Moved to ADPS9960
    This seems to have some nicer characteristics

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/TestAdafruitRangeSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/TestAdafruitRangeSensor.java
@@ -4,35 +4,32 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.hardware.AdafruitADPS9960;
 import org.firstinspires.ftc.teamcode.hardware.AdafruitBiColorMatrix;
 import org.firstinspires.ftc.teamcode.hardware.AdafruitVCLN4010;
 
-@Autonomous(name = "Sensor: Adafruit range sensor", group = "Test")
+@Autonomous(name = "Adafruit range sensor", group = "Test")
 public class TestAdafruitRangeSensor extends LinearOpMode {
 
-    AdafruitVCLN4010 rangeSensor;
-    AdafruitBiColorMatrix display;
+    AdafruitADPS9960 rangeSensor;
 
     @Override public void runOpMode() {
 
         // get a reference to our compass
-        rangeSensor = hardwareMap.get(AdafruitVCLN4010.class, "sensor_range");
-        //display = hardwareMap.get(AdafruitBiColorMatrix.class, "led_matrix");
-//        AdafruitBiColorMatrix.Graphix graphix = display.getGraphix();
-//        graphix.fillScreen(graphix.YELLOW);
-//        graphix.display();
+        rangeSensor = hardwareMap.get(AdafruitADPS9960.class, "sensor_range");
 
         // wait for the start button to be pressed
         waitForStart();
 
         while (opModeIsActive()) {
-            telemetry.addData("raw optical", rangeSensor.getRawLightDetected());
+            telemetry.addData("raw optical", rangeSensor.readProximityData());
             telemetry.addData("mm", "%.2f mm", rangeSensor.getDistance(DistanceUnit.MM));
+            telemetry.addData("ambient", "%d", rangeSensor.readAmbientLightLevel());
+            telemetry.addData("color", "(%d, %d, %d)",
+                    rangeSensor.readRedData(),
+                    rangeSensor.readGreenData(),
+                    rangeSensor.readBlueData());
             telemetry.update();
-//            graphix.fillScreen(graphix.GREEN);
-//            graphix.fillRect(0,0, 8, (int)rangeSensor.getDistance(DistanceUnit.MM), graphix.RED);
-//            graphix.display();
-
             sleep(1);
         }
     }
