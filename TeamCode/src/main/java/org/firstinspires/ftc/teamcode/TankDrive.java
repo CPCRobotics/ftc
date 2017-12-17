3900035cc87af8ea22d2629196eda0ac1f2dfdaf
Samuel Hunter <lawful.lazy@gmail.com>
2017-12-16 16:15:05 -0800

    Add rudimentary JudgeMode for evaluation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
@@ -50,7 +50,7 @@ public class TankDrive extends OpMode {
     private Tilerunner tilerunner = new Tilerunner();
 
     private double whackerPosition = 0;
-    private static final double WHACKER_CONTROL_SPEED = 0.05;
+    public static final double WHACKER_CONTROL_SPEED = 0.05;
 
     private double speedLeft = 0;
     private double speedRight = 0;
@@ -58,7 +58,7 @@ public class TankDrive extends OpMode {
     private boolean easyModeTriggered = false;
     private ElapsedTime timeSinceEasyModeTriggered = new ElapsedTime();
 
-    private static final double JOYSTICK_THRESHOLD = 0.2;
+    public static final double JOYSTICK_THRESHOLD = 0.2;
 
     private long gamepad2Timestamp;
     private ElapsedTime gamepad2Time = new ElapsedTime();
@@ -75,11 +75,11 @@ public class TankDrive extends OpMode {
         return (targetPower - currentSpeed) / SPEED_GAIN + currentSpeed;
     }
 
-    private double calculateLiftSpeed(double joystickPower) {
+    public static double calculateLiftSpeed(double joystickPower) {
         return (joystickPower * joystickPower) * Math.signum(joystickPower);
     }
 
-    private static double withinRange(double min, double max, double x) {
+    public static double withinRange(double min, double max, double x) {
         return Math.min(max, Math.max(min, x));
     }
 
