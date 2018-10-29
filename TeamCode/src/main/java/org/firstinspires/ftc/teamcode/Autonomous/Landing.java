f3fe6c1627f0cc780c766d86db92f71bfe65ad75
Richard Elmore <richardelmore@live.com>
2018-10-28 19:56:48 -0700

    Clean up Landing class, add comments so it's a better example.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Landing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Landing.java
@@ -5,8 +5,17 @@ import com.qualcomm.robotcore.hardware.DigitalChannel;
 
 import org.firstinspires.ftc.teamcode.Util.OpModeKeeper;
 
+/**
+ * Code to handle the Landing operation of autonomous for Rover Ruckus.
+ */
 public class Landing {
 
+	/**
+	 * Lower the robot to the mat and unlatch from the lander.
+	 *
+	 * @param motor - DcMotor object for the robot lift mechanism.
+	 * @param upperLimit - DigitalChannel object used to read the state of the lifts upper limit switch.
+	 */
     public static void Land(DcMotor motor, DigitalChannel upperLimit)
     {
     	// Start raising the lift (which lowers the robot).
